af18656368f3f98d377f2ebdf2005b4b58fce4a4
DJ. Harris <apparitiondj@outlook.com>
2024-05-29 19:00:44 -0400

    Factorial implemented

--- factorials/src/main.rs
+++ factorials/src/main.rs
@@ -1,3 +1,17 @@
+// Factorial excersize
+
 fn main() {
-    println!("Hello, world!");
+    for n in 0..22 {
+        println!("{}! = {}", n, factorial(n));
+    }
+}
+
+fn factorial(n: i64) -> i64 {
+
+    assert!(n >= 0, "n must be positive");
+    if n == 0 {
+        1
+    } else {
+        n * factorial(n - 1)
+    }
 }
